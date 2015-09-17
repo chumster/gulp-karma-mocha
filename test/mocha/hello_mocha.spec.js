f72bb392e85dd49c22fcd7ce1f8a5b0596fe591a
Stephen Chumley <Chumley@Edgecase-Chumley-MBP.local>
2015-09-17 10:58:00 -0500

    updates to test files

--- test/mocha/hello_mocha.spec.js
+++ test/mocha/hello_mocha.spec.js
@@ -8,6 +8,6 @@ describe('greeter', function () {
 // Should Fail
 describe('greeter', function () {
   it('should say Hello to the World', function () {
-    expect(greet('World')).to.equal('Yo! Hello, World!');
+    expect(greet('World')).to.equal('Hello, World!');
   });
 });
